a9b3811ed8a4cb91fa7e918bffb4239b341bb68d
jbnilles <jbnilles24@gmail.com>
2020-08-14 15:09:14 -0700

    refactor code to an acceptable standard

--- js/scripts.js
+++ js/scripts.js
@@ -1,254 +1,187 @@
-$(document).ready(function(){
-
-$('#nextButton').click(function(){
-  $('#questionCard').hide();
-  let currentQuestion = getCurrentQuestion($('#questionCard .card-title').text());
-  let nextQuestion = getNextQuestion($('#questionCard .card-title').text());
-  
-  $('#questionCard .card-title').text(QUESTIONS[nextQuestion]);
-  $('#questionCard .card-header').text("Question " + (nextQuestion + 1));
-  writeToANSWERS(currentQuestion);
-  // if(!isNaN(parseInt($('input[type="radio"][name="questionRadio"]:checked').val()))){
-  //   ANSWERS[currentQuestion ] = parseInt($('input[type="radio"][name="questionRadio"]:checked').val());
-  
-  // }
-  setRadioButton(nextQuestion)
-  //ANSWERS[currentQuestion ] = parseInt($('input[type="radio"][name="questionRadio"]:checked').val());
-  //$('#questionCard #option'+ ANSWERS[nextQuestion]).prop("checked",true);
-  // if(ANSWERS[nextQuestion])
-  // {
-  //   $('#questionCard #option'+ ANSWERS[nextQuestion]).prop("checked",true);
-
-  // }
-  // else
-  // {
-  //   $('input[type="radio"][name="questionRadio"]:checked').prop("checked",false);
-
-  // }
-  checkIfButtonsNeeded(nextQuestion);
-
-  $('#questionCard').show();
-});
+$(document).ready(function (){
 
 
-$('#previousButton').click(function(){
+  $('#nextButton').click(function (){
+    $('#questionCard').hide();
+    let currentQuestion = getCurrentQuestion($('#questionCard .card-title').text());
+    let nextQuestion = getNextQuestion($('#questionCard .card-title').text());
+    writeToCard(nextQuestion);
+    writeToANSWERS(currentQuestion);
+    setRadioButton(nextQuestion)
+    checkIfButtonsNeeded(nextQuestion);
+    $('#questionCard').show();
+  });
+
+
+$('#previousButton').click(function (){
   $('#questionCard').hide();
   let currentQuestion = getCurrentQuestion($('#questionCard .card-title').text());
   let previousQuestion = getPreviousQuestion($('#questionCard .card-title').text());
-  $('#questionCard .card-title').text(QUESTIONS[previousQuestion]);
-  $('#questionCard .card-header').text("Question " + (previousQuestion + 1));
+  writeToCard(previousQuestion);
   writeToANSWERS(currentQuestion);
-  // if(!isNaN(parseInt($('input[type="radio"][name="questionRadio"]:checked').val()))){
-  //   ANSWERS[currentQuestion ] = parseInt($('input[type="radio"][name="questionRadio"]:checked').val());
-    
-  // }//ANSWERS[currentQuestion ] = parseInt($('input[type="radio"][name="questionRadio"]:checked').val());
   setRadioButton(previousQuestion)
-  // if(ANSWERS[previousQuestion])
-  // {
-  //   $('#questionCard #option'+ ANSWERS[previousQuestion]).prop("checked",true);
-
-  // }
-  // else
-  // {
-    
-  //   $('input[type="radio"][name="questionRadio"]:checked').prop("checked",false);
-
-  // }
-
   checkIfButtonsNeeded(previousQuestion);
   $('#questionCard').show();
   });
 
 
-
-$('#submitButton').click(function(){
-  //$('#questionCard .card-title').text(QUESTIONS[getPreviousQuestion($('#questionCard .card-title').text())]);
-  
+$('#submitButton').click(function (){
   let currentQuestion = getCurrentQuestion($('#questionCard .card-title').text());
-  //alert(parseInt($('input[type="radio"][name="questionRadio"]:checked').val()));
-  // 
   writeToANSWERS(currentQuestion);
-  //alert(ANSWERS);
-  if(checkAllQuestionsAnswered())
-  {
-    $('#questionCard').hide();
-    decideResult(addAnswers()); 
-  }
-  else
-  {
+  $('#questionCard').hide();
+  if(checkAllQuestionsAnswered()) {
+    showResult(addAnswers()); 
+  } else {
    let unanswered =  getUnansweredQuestion();
-   alert("Need an answer for question"+ unanswered+": "+  QUESTIONS[unanswered - 1] );
-   $('#questionCard .card-title').text(QUESTIONS[unanswered - 1]);
-   $('#questionCard .card-header').text("Question " + (unanswered));
+   alert("Need an answer for question" + unanswered + ": " + QUESTIONS[unanswered - 1] );
+   writeToCard(unanswered - 1);
    setRadioButton(unanswered - 1);
    checkIfButtonsNeeded(unanswered -1);
-   //$('#questionCard .card-title').text(QUESTIONS[getPreviousQuestion($('#questionCard .card-title').text())]);
   }
+  $('#questionCard').show();
   });
-  $('#restartButton').click(function(){
+
+  $('#restartButton').click(function (){
     $('#cCard').hide();
     $('#jsCard').hide();
     $('#javaCard').hide();
     $('#introCard').hide();
-    ANSWERS = [0,0,0,0,0];
-    $('#questionCard .card-title').text(QUESTIONS[0]);
-    $('#questionCard .card-header').text("Question 1");
-    $('input[type="radio"][name="questionRadio"]:checked').prop("checked",false);
+    $('#questionCard').hide();
+    resetANSWERS()
+    writeToCard(0);
+    $('input[type="radio"][name="questionRadio"]:checked').prop("checked", false);
     $(this).text("Restart Quiz");
     checkIfButtonsNeeded(0);
     $('#questionCard').show();
   });
-  
 
 });
 
-const QUESTIONS = ["Do you prefer to work with websites?","Do you want to avoid working with directly with machine language?","Do you prefer to work with api's?","Does design intrest you?","Do you prefer to work with a live editor(console)?"];
-let ANSWERS =[0,0,0,0,0];
+const QUESTIONS = ["Do you prefer to work with websites?", "Do you want to avoid working with directly with machine language?", "Do you prefer to work with api's?", "Does design intrest you?", "Do you prefer to work with a live editor(console)?"];
+let ANSWERS = [0, 0, 0, 0, 0];
 
-function getNextQuestion(currentQuestion) {
+function getNextQuestion (currentQuestion) {
   let i = 0;
-  for( i = 0; i < QUESTIONS.length;i++)
+  for ( i = 0; i < QUESTIONS.length; i++)
   {
-    if(QUESTIONS[i] === currentQuestion)
-    {
-      if(i === QUESTIONS.length - 1 )
-      {
+    if (QUESTIONS[i] === currentQuestion) {
+      if (i === QUESTIONS.length - 1 ) {
         return 0;
+      } else {
+        return i + 1;
       }
-      else
-      {
-        return i+1;
-      }
-      
     }
   }
   return nextQuestion;
 }
-function getPreviousQuestion(currentQuestion) {
+
+function getPreviousQuestion (currentQuestion) {
   let i = 0;
-  for( i = 0; i < QUESTIONS.length;i++)
+  for ( i = 0; i < QUESTIONS.length; i++)
   {
-    if(QUESTIONS[i] === currentQuestion)
-    {
-      if(i === 0)
-      {
+    if (QUESTIONS[i] === currentQuestion) {
+      if (i === 0) {
         return QUESTIONS.length - 1;
+      } else {
+        return i - 1;
       }
-      else
-      {
-        return i-1;
-      }
-      
     }
   }
   return nextQuestion;
 }
-function getCurrentQuestion(currentQuestion) {
+
+function getCurrentQuestion (currentQuestion) {
   let i = 0;
-  for( i = 0; i < QUESTIONS.length;i++)
+  for (i = 0; i < QUESTIONS.length; i++)
   {
-    if(QUESTIONS[i] === currentQuestion)
-    {
+    if (QUESTIONS[i] === currentQuestion) {
       return i;
-      
     }
   }
   return -1;
 }
 
-function checkAllQuestionsAnswered() {
+function checkAllQuestionsAnswered () {
   let i =0;
-  for(i = 0; i <ANSWERS.length; i++)
+  for (i = 0; i <ANSWERS.length; i++)
   {
-    //alert("checkallanswered");
-    if (ANSWERS[i] === 0)
-    {
+    if (ANSWERS[i] === 0) {
       return false;
     }
-    
-    
-    
   }
   return true;
 }
-function getUnansweredQuestion() {
+
+function getUnansweredQuestion () {
   let i =0;
-  for(i = 0; i <ANSWERS.length; i++)
+  for (i = 0; i <ANSWERS.length; i++)
   {
-    if (ANSWERS[i] === 0)
-    {
+    if (ANSWERS[i] === 0) {
       return i + 1;
     }
-    
   }
   return -1;
 }
 
-function addAnswers() {
+function addAnswers () {
   let i = 0;
   let j = 0;
-  //alert(ANSWERS.length + "answerlength");
   for (i = 0; i < ANSWERS.length; i++)
   {
     j = j + ANSWERS[i];
   }
   return j;
 }
-function decideResult(answersValue) {
-  //alert(answersValue);
-  if(answersValue <= 8)
-  {
+
+function showResult (answersValue) {
+  if (answersValue <= 10) {
     $("#cCard").show();
-    //alert("option1");
-  }
-  else if(answersValue > 8 && answersValue <= 16)
-  {
+  } else if (answersValue > 10 && answersValue <= 19) {
     $("#javaCard").show();
-    //alert("option2");
-  }
-  else
-  {
+  } else {
     $("#jsCard").show();
-    //alert("option3");
   }
+  return;
 }
-function setRadioButton(question) {
-  if(ANSWERS[question])
-  {
-    $('#questionCard #option'+ ANSWERS[question]).prop("checked",true);
 
+function setRadioButton (question) {
+  if (ANSWERS[question]) {
+    $('#questionCard #option' + ANSWERS[question]).prop("checked", true);
+  } else {
+    $('input[type="radio"][name="questionRadio"]:checked').prop("checked", false);
   }
-  else
-  {
-    $('input[type="radio"][name="questionRadio"]:checked').prop("checked",false);
-
-  }
+  return;
 }
-function writeToANSWERS(currentQuestion) {
-  if(!isNaN(parseInt($('input[type="radio"][name="questionRadio"]:checked').val()))){
+
+function writeToANSWERS (currentQuestion) {
+  if (!isNaN(parseInt($('input[type="radio"][name="questionRadio"]:checked').val()))) {
     ANSWERS[currentQuestion ] = parseInt($('input[type="radio"][name="questionRadio"]:checked').val());
-    
   }
+  return;
 }
-  function checkIfButtonsNeeded(question) {
-    if(question === 0)
-    {
-      $("#previousButton").hide();
-      $("#nextButton").show();
 
-    }
-    else if(question === 4)
-    {
-      $("#previousButton").show();
-      $("#nextButton").hide();
-    }
-    else
-    {
-      $("#previousButton").show();
-      $("#nextButton").show();
-    }
-    return;
-    
+function checkIfButtonsNeeded (question) {
+  if (question === 0) {
+    $("#previousButton").hide();
+    $("#nextButton").show();
+  } else if(question === 4) {
+    $("#previousButton").show();
+    $("#nextButton").hide();
+  } else {
+    $("#previousButton").show();
+    $("#nextButton").show();
+  }
+  return;
   }
 
+  function writeToCard (questionIndex) {
+    $('#questionCard .card-title').text(QUESTIONS[questionIndex]);
+    $('#questionCard .card-header').text("Question " + (questionIndex + 1));
+    return;
+  }
+
+  function resetANSWERS () {
+    ANSWERS = [0, 0, 0, 0, 0];
+    return;
+  }
 
