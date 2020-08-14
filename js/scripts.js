b22edcce3e3d3574a32d4bf72cfae4c35ae00058
jbnilles <jbnilles24@gmail.com>
2020-08-14 09:17:53 -0700

    add dom traversal for dynamic editing in js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+$('#nextButton').click(function(){
+//this.parentNode.parentNode.css('background','yellow'));
+$('.card').css('background','yellow');
+
+});
+$('#previousButton').click(function(){
+  $('.card').css('background','blue');
+  
+  });
+
+
+});
